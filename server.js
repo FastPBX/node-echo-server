1cb334efb695c5d8215cbcdc135e5d03fa6dda20
David Melo <davidmelo@desmdesigns.com>
2019-07-12 13:47:08 -0400

    Adds Pod IP Debug

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ let dgram = require('dgram');
 let PORT = 55510;
 let HOST = '0.0.0.0';
 
+const MY_HOST_IP = process.env.MY_HOST_IP;
+const MY_NODE_NAME = process.env.MY_NODE_NAME;
+const MY_POD_NAME = process.env.MY_POD_NAME;
+const MY_POD_NAMESPACE = process.env.MY_POD_NAMESPACE;
+const MY_POD_IP = process.env.MY_POD_IP;
+const MY_POD_SERVICE_ACCOUNT = process.env.MY_POD_SERVICE_ACCOUNT;
+
 let server = [];
 
 for (let i = 0; i < 10; i++) {
@@ -22,7 +29,7 @@ for (let i = 0; i < 10; i++) {
     server[i].on('message', function (message, remote) {
         console.log('Data received from client : ' + message.toString());
 
-        this.send(new Buffer(message), remote.port, remote.address, function (err, bytes) {
+        this.send(new Buffer(message + '|' + MY_HOST_IP + '|' + MY_POD_IP), remote.port, remote.address, function (err, bytes) {
             if (err) throw err;
             console.log(`UDP message sent to ${remote.address}:${remote.port}`);
         });
